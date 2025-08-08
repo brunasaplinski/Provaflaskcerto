ea3cba016ec22207c137718b407de64613f81861
BRUNA SAPLINSKI <bruna_saplinski@estudante.sesisenai.org.br>
2025-08-08 08:31:07 -0300

    att

--- app/models.py
+++ app/models.py
@@ -7,6 +7,14 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(user_id)
 
+
+class User(db.Model, UserMixin):
+    id=db.Column(db.Integer, primary_key=True)
+    nome=db.Column(db.String, nullable=True)
+    sobrenome=db.Column(db.String, nullable=True)
+    email=db.Column(db.String, nullable=True)
+    senha=db.Column(db.String,nullable=True)
+
 class Contato(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.DateTime, default=datetime.now())
